16675cfc76f65798baa3a234ae04e73128d0e13f
Irie Aoi <eai@mizle.net>
2022-07-16 04:21:11 +0900

    original素材に対応

--- components/GridTable.tsx
+++ components/GridTable.tsx
@@ -1,6 +1,58 @@
-import React from "react";
+import React, {
+  DetailedHTMLProps,
+  ImgHTMLAttributes,
+  ReactEventHandler,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { EventStories } from "../pages/events/index.page.server";
 import { cn } from "./utils";
+
+const UnitImage = ({
+  src,
+  ...props
+}: DetailedHTMLProps<
+  ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+>) => {
+  const [hasRendered, setHasRendered] = useState(false);
+  const ref = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (ref.current && hasRendered) {
+      ref.current!.src = src || "";
+    }
+  }, [src, hasRendered]);
+
+  useEffect(() => {
+    setHasRendered(true);
+  }, []);
+
+  /**
+   * 本家素材がなかったらoriginal素材のパスをsrcに設定する
+   */
+  const onError1 = (e: SyntheticEvent<HTMLImageElement>) => {
+    console.log("onerror1", e);
+    //@ts-ignore
+    e.currentTarget.onerror = onError2;
+    const arr = e.currentTarget.src.split("/");
+    arr.splice(3, 0, "original");
+    e.currentTarget.src = arr.join("/");
+  };
+  /**
+   * originalも見つからなかった時のフォールバック
+   */
+  const onError2 = (e: SyntheticEvent<HTMLImageElement>) => {
+    console.log("onerror2", e);
+    e.currentTarget.onerror = null;
+    e.currentTarget.src =
+      "https://cdn.laoplus.net/formationicon/FormationIcon_empty.webp";
+  };
+
+  return <img {...props} src={src} ref={ref} onError={onError1} />;
+};
 
 export function GridTableRenderer({
   eventIndexStr,
@@ -43,7 +95,7 @@ export function GridTableRenderer({
             <div className="grid grid-cols-3 gap-1">
               {s.StartCutsceneCharcters.map((c, i) => {
                 return (
-                  <img
+                  <UnitImage
                     key={i}
                     title={c.name}
                     alt={c.name}
@@ -55,12 +107,7 @@ export function GridTableRenderer({
                         .replace("_Commu", "_N") +
                       ".webp"
                     }
-                    className="aspect-square rounded-sm"
-                    onError={({ currentTarget }) => {
-                      currentTarget.onerror = null; // prevents looping
-                      currentTarget.src =
-                        "https://cdn.laoplus.net/formationicon/FormationIcon_empty.webp";
-                    }}
+                    className="aspect-square w-10 rounded-sm"
                   />
                 );
               })}
