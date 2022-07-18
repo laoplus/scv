b923b860ca75ae1fd46efa011cbc1e62d6b0fb1b
Irie Aoi <eai@mizle.net>
2022-07-19 03:21:23 +0900

    main 2

--- pages/main/chapterDetail.page.server.tsx
+++ pages/main/chapterDetail.page.server.tsx
@@ -11,60 +11,63 @@ export async function onBeforeRender({ routeParams }: PageContextBuiltIn) {
   const { chapters, stages } = tables;
   const sceneCharacters = await createSceneCharacters();
 
-  const mapChapters = chapters.filter(
+  const chapter = chapters.find(
     (c) =>
       c.GameModeType === 0 && c.Chapter_IDX === Number(routeParams.chapterIndex)
   );
 
-  // eventsにchapterつめる
-  const chaptersWithStages = mapChapters.map((c) => {
-    const ChapterStages = stages
-      .filter((stage) => stage.ChapterIndex === c.Key)
-      .map((stage) => ({
-        StageName: stage.StageName,
-        StageDesc: stage.StageDesc,
-        StageIdxString: stage.StageIdxString,
-        StageSubType: stage.StageSubType,
-        StageSubTypeStr: (() => {
-          switch (stage.StageSubType) {
-            case 0:
-              return "NORMAL" as const;
-            case 1:
-              return "SUB" as const;
-            case 2:
-              return "EX" as const;
-          }
-        })(),
-        StagePos: stage.Stage_Pos,
-        StartCutsceneIndex: stage.StartCutsceneIndex,
-        StartCutsceneCharcters: getSceneCharacters({
-          sceneCharacters,
-          cutsceneIndex: stage.StartCutsceneIndex,
-        }),
-        EndCutsceneIndex: stage.EndCutsceneIndex,
-        EndCutsceneCharcters: getSceneCharacters({
-          sceneCharacters,
-          cutsceneIndex: stage.EndCutsceneIndex,
-        }),
-        MidCutsceneIndex: stage.MidCutsceneIndex,
-        hasCutscene:
-          stage.StartCutsceneIndex !== "0" ||
-          stage.EndCutsceneIndex !== "0" ||
-          stage.MidCutsceneIndex[0] !== "0",
-      }));
+  if (!chapter) {
+    throw new Error("Chapter not found");
+  }
 
-    return {
-      ...c,
-      ChapterStages: ChapterStages,
-    };
-  });
+  console.log(chapter);
 
-  // console.log(chaptersWithStages);
+  const ChapterStages = stages
+    .filter((stage) => stage.ChapterIndex === chapter.Key)
+    .map((stage) => ({
+      StageName: stage.StageName,
+      StageDesc: stage.StageDesc,
+      StageIdxString: stage.StageIdxString,
+      StageSubType: stage.StageSubType,
+      StageSubTypeStr: (() => {
+        switch (stage.StageSubType) {
+          case 0:
+            return "NORMAL" as const;
+          case 1:
+            return "SUB" as const;
+          case 2:
+            return "EX" as const;
+        }
+      })(),
+      StagePos: stage.Stage_Pos,
+      StartCutsceneIndex: stage.StartCutsceneIndex,
+      StartCutsceneCharcters: getSceneCharacters({
+        sceneCharacters,
+        cutsceneIndex: stage.StartCutsceneIndex,
+      }),
+      EndCutsceneIndex: stage.EndCutsceneIndex,
+      EndCutsceneCharcters: getSceneCharacters({
+        sceneCharacters,
+        cutsceneIndex: stage.EndCutsceneIndex,
+      }),
+      MidCutsceneIndex: stage.MidCutsceneIndex,
+      hasCutscene:
+        stage.StartCutsceneIndex !== "0" ||
+        stage.EndCutsceneIndex !== "0" ||
+        stage.MidCutsceneIndex[0] !== "0",
+    }));
+
+  // return
+
+  console.log(ChapterStages);
 
   return {
     pageContext: {
       pageProps: {
-        chapteres: chaptersWithStages,
+        chapter: {
+          ...chapter,
+          ChapterStages: ChapterStages,
+        },
       },
     },
   };
@@ -73,6 +76,6 @@ export async function onBeforeRender({ routeParams }: PageContextBuiltIn) {
 export const prerender = async () => {
   const chapters = tables.chapters.filter((c) => c.GameModeType === 0);
 
-  const chapterIndex = [...new Set(chapters.map((e) => e.GameModeType))];
-  return chapterIndex.map((i) => `/events/${i}`);
+  const chapterIndex = [...new Set(chapters.map((e) => e.Chapter_IDX))];
+  return chapterIndex.map((i) => `/main/${i}`);
 };
--- pages/main/chapterDetail.page.tsx
+++ pages/main/chapterDetail.page.tsx
@@ -6,22 +6,20 @@ type PageProps = Awaited<
   ReturnType<typeof onBeforeRender>
 >["pageContext"]["pageProps"];
 
-export function Page({ chapteres }: PageProps) {
+export function Page({ chapter }: PageProps) {
   return (
     <>
       <h1 className="mb-4 text-6xl uppercase">Main Scenes</h1>
 
       <div className="flex flex-col gap-8 p-2">
-        {chapteres.map((c) => (
-          <div
-            key={c.Chapter_IDX}
-            id={`${c.Chapter_IDX}`}
-            className="flex flex-col gap-4"
-          >
-            <h2 className="sticky top-0 text-2xl">{c.ChapterName}</h2>
-            <StageGridTable eventIndexStr="main" stages={c.ChapterStages} />
-          </div>
-        ))}
+        <div
+          key={chapter.Chapter_IDX}
+          id={`${chapter.Chapter_IDX}`}
+          className="flex flex-col gap-4"
+        >
+          <h2 className="sticky top-0 text-2xl">{chapter.ChapterName}</h2>
+          <StageGridTable eventIndexStr="main" stages={chapter.ChapterStages} />
+        </div>
       </div>
     </>
   );
