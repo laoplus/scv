910618faabb86a6070eb007f17b4fae9350a3eb3
Irie Aoi <eai@mizle.net>
2022-07-19 21:46:06 +0900

    エラー画面にスタイルをつける

--- renderer/_error.page.tsx
+++ renderer/_error.page.tsx
@@ -5,17 +5,17 @@ export { Page };
 function Page({ is404 }: { is404: boolean }) {
   if (is404) {
     return (
-      <>
-        <h1>404 Page Not Found</h1>
+      <div className="my-12 text-center">
+        <h1 className="mb-4 text-4xl">404 Page Not Found</h1>
         <p>This page could not be found.</p>
-      </>
+      </div>
     );
   } else {
     return (
-      <>
-        <h1>500 Internal Server Error</h1>
+      <div className="my-12 text-center">
+        <h1 className="mb-4 text-4xl">500 Internal Server Error</h1>
         <p>Something went wrong.</p>
-      </>
+      </div>
     );
   }
 }
