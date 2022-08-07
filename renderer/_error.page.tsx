1d9e11d5fcf04536a57aa8fab8f1229fe7a344c1
Irie Aoi <eai@mizle.net>
2022-08-07 16:29:04 +0900

    errorページの見た目を修正

--- renderer/_error.page.tsx
+++ renderer/_error.page.tsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { Heading } from "../components/Heading";
 
 export { Page };
 
 function Page({ is404 }: { is404: boolean }) {
   if (is404) {
     return (
-      <div className="my-12 text-center">
-        <h1 className="mb-4 text-4xl">404 Page Not Found</h1>
-        <p>This page could not be found.</p>
+      <div className="my-12 flex flex-col gap-4 px-2 text-center">
+        <img
+          src="https://lo.swaytwig.com/assets/webp/sticker/Manme03_3.webp"
+          className="m-auto h-36 w-36"
+        />
+        <Heading level={1} classname="px-0 py-0">
+          404 Page Not Found
+        </Heading>
+        <p>This page no longer exists, may have been deleted or moved.</p>
       </div>
     );
   } else {
     return (
-      <div className="my-12 text-center">
-        <h1 className="mb-4 text-4xl">500 Internal Server Error</h1>
+      <div className="my-12 flex flex-col gap-4 px-2 text-center">
+        <img
+          src="https://lo.swaytwig.com/assets/webp/sticker/Diyap07_1.webp"
+          className="m-auto h-36 w-36"
+        />
+        <Heading level={1} classname="px-0 py-0">
+          500 Internal Server Error
+        </Heading>
         <p>Something went wrong.</p>
       </div>
     );
