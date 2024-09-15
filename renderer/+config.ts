f16a13d66c93f5f1bf737b467a921feb92fded10
Eai <eai@mizle.net>
2024-09-15 23:40:22 +0900

    set hooks timeout

--- renderer/+config.ts
+++ renderer/+config.ts
@@ -2,4 +2,10 @@ import type { Config } from "vike/types";
 
 export default {
     clientRouting: true,
+    hooksTimeout: {
+        onBeforeRender: {
+            warning: 10 * 1000,
+            error: Infinity,
+        },
+    },
 } satisfies Config;
