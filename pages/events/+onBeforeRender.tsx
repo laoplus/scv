db47aade34bb1d19259ed51c8b875e7dbe88e4bf
Eai <eai@mizle.net>
2025-02-24 22:50:06 +0900

    refactor: CharacterIconの構造をシンプルに

--- pages/events/+onBeforeRender.tsx
+++ pages/events/+onBeforeRender.tsx
@@ -2,100 +2,32 @@ import { publicEvents } from "./details/+onBeforeRender";
 
 export async function onBeforeRender() {
   const CDN_BASE_URL = import.meta.env.VITE_CDN_BASE_URL;
-  const CharacterIcon = [
-    {
-      Event_Category: "Open_Event01",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Sowan_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event02",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Triaina_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event03",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Baekto_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event04",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Circe_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event05",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_DrM_NS1.webp`,
-    },
-    {
-      Event_Category: "Open_Event06",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Aurora_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event07",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_HighElven_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event08",
-      CharacterIcon: `${CDN_BASE_URL}/original/formationicon/FormationIcon_BR_TomoeKIN_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event09",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Machina_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event10",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Hirume_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event11",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Sleipnir_NS1.webp`,
-    },
-    {
-      Event_Category: "Open_Event12",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Angel_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event13",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_JangHwa_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event14",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Mnemosyne_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event15",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_DrM_NS2.webp`,
-    },
-    {
-      Event_Category: "Open_Event16",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_ConstantiaS2_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event17",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Khan_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event18",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Spriggan_NS1.webp`,
-    },
-    {
-      Event_Category: "Open_Event19",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_BlindPrincess_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event20",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Alcyone_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event22",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Sarena_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event23",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Koyori_N.webp`,
-    },
-    {
-      Event_Category: "Open_Event25",
-      CharacterIcon: `${CDN_BASE_URL}/original/formationicon/FormationIcon_3P_Jiseok_N.webp`,
-    },
-  ];
+
+  const CharacterIcon: Record<string, string> = {
+    Open_Event01: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Sowan_N.webp`,
+    Open_Event02: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Triaina_N.webp`,
+    Open_Event03: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Baekto_N.webp`,
+    Open_Event04: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Circe_N.webp`,
+    Open_Event05: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_DrM_NS1.webp`,
+    Open_Event06: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Aurora_N.webp`,
+    Open_Event07: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_HighElven_N.webp`,
+    Open_Event08: `${CDN_BASE_URL}/original/formationicon/FormationIcon_BR_TomoeKIN_N.webp`,
+    Open_Event09: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Machina_N.webp`,
+    Open_Event10: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Hirume_N.webp`,
+    Open_Event11: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Sleipnir_NS1.webp`,
+    Open_Event12: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Angel_N.webp`,
+    Open_Event13: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_JangHwa_N.webp`,
+    Open_Event14: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_Mnemosyne_N.webp`,
+    Open_Event15: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_DrM_NS2.webp`,
+    Open_Event16: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_ConstantiaS2_N.webp`,
+    Open_Event17: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Khan_N.webp`,
+    Open_Event18: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Spriggan_NS1.webp`,
+    Open_Event19: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_BlindPrincess_N.webp`,
+    Open_Event20: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Alcyone_N.webp`,
+    Open_Event22: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Sarena_N.webp`,
+    Open_Event23: `${CDN_BASE_URL}/formationicon/FormationIcon_DS_Koyori_N.webp`,
+    Open_Event25: `${CDN_BASE_URL}/original/formationicon/FormationIcon_3P_Jiseok_N.webp`,
+  };
 
   const events = (() => {
     const map = new Map<string, (typeof publicEvents)[number]>();
@@ -108,9 +40,7 @@ export async function onBeforeRender() {
   })();
   const eventsWithIcon = events.map((c) => ({
     ...c,
-    CharacterIcon: CharacterIcon.find(
-      (i) => i.Event_Category === c.Event_Category,
-    )?.CharacterIcon,
+    CharacterIcon: CharacterIcon[c.Event_Category],
   }));
 
   return {
--- pages/main/+onBeforeRender.tsx
+++ pages/main/+onBeforeRender.tsx
@@ -4,60 +4,24 @@ export async function onBeforeRender() {
   const CDN_BASE_URL = import.meta.env.VITE_CDN_BASE_URL;
   const mainChapters = tables.chapters.filter((c) => c.GameModeType === 0);
 
-  const CharacterIcon = [
-    {
-      Key: "Chapter_01N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_ConstantiaS2_N.webp`,
-    },
-    {
-      Key: "Chapter_02N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Amy_N.webp`,
-    },
-    {
-      Key: "Chapter_03N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Marie_N.webp`,
-    },
-    {
-      Key: "Chapter_04N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_AGS_Aeda_N.webp`,
-    },
-    {
-      Key: "Chapter_05N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Scathy_N.webp`,
-    },
-    {
-      Key: "Chapter_06N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Labiata_N.webp`,
-    },
-    {
-      Key: "Chapter_07N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_InvDragon_N.webp`,
-    },
-    {
-      Key: "Chapter_08N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_LemonadeAlpha_N.webp`,
-    },
-    {
-      Key: "Chapter_09N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_LemonadeGamma_N.webp`,
-    },
-    {
-      Key: "Chapter_10N",
-      CharacterIcon: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Vargr_N.webp`,
-    },
-    {
-      Key: "Chapter_11N",
-      CharacterIcon: `${CDN_BASE_URL}/original/formationicon/FormationIcon_PECS_LemonadeDelta_N.webp`,
-    },
-  ];
+  const CharacterIcon: Record<string, string> = {
+    Chapter_01N: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_ConstantiaS2_N.webp`,
+    Chapter_02N: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Amy_N.webp`,
+    Chapter_03N: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Marie_N.webp`,
+    Chapter_04N: `${CDN_BASE_URL}/formationicon/FormationIcon_AGS_Aeda_N.webp`,
+    Chapter_05N: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Scathy_N.webp`,
+    Chapter_06N: `${CDN_BASE_URL}/formationicon/FormationIcon_3P_Labiata_N.webp`,
+    Chapter_07N: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_InvDragon_N.webp`,
+    Chapter_08N: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_LemonadeAlpha_N.webp`,
+    Chapter_09N: `${CDN_BASE_URL}/formationicon/FormationIcon_PECS_LemonadeGamma_N.webp`,
+    Chapter_10N: `${CDN_BASE_URL}/formationicon/FormationIcon_BR_Vargr_N.webp`,
+    Chapter_11N: `${CDN_BASE_URL}/original/formationicon/FormationIcon_PECS_LemonadeDelta_N.webp`,
+  };
 
-  const chapters = mainChapters.map((c) => {
-    const icon = CharacterIcon.find((i) => i.Key === c.Key)?.CharacterIcon;
-    return {
-      ...c,
-      CharacterIcon: icon,
-    };
-  });
+  const chapters = mainChapters.map((c) => ({
+    ...c,
+    CharacterIcon: CharacterIcon[c.Key],
+  }));
 
   return {
     pageContext: {
